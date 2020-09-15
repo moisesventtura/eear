863e71d48d53c8311280b6b39393bf0d649530a9
moisesventtura <moisesventura15@gmail.com>
2020-09-14 21:31:54 -0300

    ok

--- ArrayAtéOTalo/parEImpar.js
+++ ArrayAtéOTalo/parEImpar.js
@@ -1,16 +1,16 @@
 function vetor(a,b){
     var soma = 0
-    par = []
+    impar = []
     k = [0]
     for(var i = a ; i < b ; i++ ){
         if(i % 2 !==0){
-            par[k] = i
+            impar[k] = i
             soma += i
             k++
         }
     }
         console.log("A soma do números ímpares é: " + soma)
-        return par
+        return impar
 }
 
 console.log(vetor(10,20))
