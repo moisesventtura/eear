1f55b31c0746daacabc20b5a49ae598ef1cfe778
moisesventtura <moisesventura15@gmail.com>
2020-09-17 00:50:48 -0300

    o pai ta cansado

--- ArrayAtéOTalo/makeArrayConsective.js
+++ ArrayAtéOTalo/makeArrayConsective.js
@@ -1,30 +1,24 @@
-
-
-function makeArrayConsecutive2(statues) {
-
-    var tam = statues.length
-
-  var intervalo = maiorValor(statues) - menorValor(statues) - tam + 1
-    console.log(intervalo)
+function makeArrayConsecutive(vetor){
+    var tamanho = vetor.length
+    var consecutivosFaltantes = maiorValor(vetor) - menorValor(vetor) - tamanho + 1
+    console.log("O total de números faltantes são: " + consecutivosFaltantes)
 }
-  function maiorValor(statues){
-    var maiorV=statues[0]
-      for (var i=0;i<statues.length;i++){
-        maiorV = maiorV < statues[i]? statues[i]:maiorV
+
+function maiorValor(vetor){
+    var maior = vetor[0]
+    for(var i = 0 ; i < vetor.length ; i++){
+        maior = maior < vetor[i] ? vetor[i] : maior
     }
-  
-  return maiorV
+    return maior
 }
-function menorValor(statues){
-  var menorV=statues[0]
-  for (i=0;i<statues.length;i++){
-        menorV = menorV > statues[i]? statues[i]:menorV
+
+function menorValor(vetor){
+    var menor = vetor[0]
+    for(i = 0 ; i < vetor.length ; i++){
+        menor = menor > vetor[i] ? vetor[i] : menor
     }
-  
-  return menorV
+    return menor
 }
-
-console.log("Maior: " + maiorValor([6,2,3,8])) 
-console.log("Menor: " + menorValor([6,2,3,8])) 
-makeArrayConsecutive2 ([6,2,3,8])
-
+makeArrayConsecutive([6,2,3,8,10,50])
+console.log("O Maior valor do array é: " + maiorValor([6,2,3,8,10,50]))
+console.log("O menor valor do Array é: " + menorValor([6,2,3,8,10,50]))
